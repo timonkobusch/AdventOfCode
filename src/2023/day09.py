6a191261c40979a68f9c8078e19b2125c59abf81
Timon Kobusch <timon_kobusch@web.de>
2023-12-19 16:09:35 +0100

    7, 9, 10

--- src/2023/day09.py
+++ src/2023/day09.py
@@ -4,12 +4,10 @@ from functools import reduce
 def p1(lines):
     ans = 0
     for line in lines:
-        nums = list(map(lambda x: int(x), line.split()))
+        nums = [int(x) for x in line.split()]
         last_list = [nums[-1]]
         while sum(nums) != 0:
-            new_nums = []
-            for i in range(1, len(nums)):
-                new_nums.append(nums[i] - nums[i-1])
+            new_nums = [nums[i] - nums[i - 1] for i in range(1, len(nums))]
             last_list.append(new_nums[-1])
             nums = new_nums
         ans += reduce(lambda a, b: a+b, last_list)
@@ -19,12 +17,10 @@ def p1(lines):
 def p2(lines):
     ans = 0
     for line in lines:
-        nums = list(map(lambda x: int(x), line.split()))
+        nums = [int(x) for x in line.split()]
         first_list = [nums[0]]
         while sum(nums) != 0:
-            new_nums = []
-            for i in range(1, len(nums)):
-                new_nums.append(nums[i] - nums[i - 1])
+            new_nums = [nums[i] - nums[i - 1] for i in range(1, len(nums))]
             first_list.insert(0, new_nums[0])
             nums = new_nums
 
