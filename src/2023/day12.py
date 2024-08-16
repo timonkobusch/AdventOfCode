36a0ff43f462461fb6e3cc2ce1d377b2c452b86f
Timon Kobusch <timon_kobusch@web.de>
2024-08-16 10:58:39 +0200

    Day 12 pt. 2 solution

--- src/2023/day12.py
+++ src/2023/day12.py
@@ -1,38 +1,62 @@
+from functools import cache
+from typing import Tuple
+
+
+# python main.py --sample true --year 2023 12
+@cache
+def possible(string, groups: Tuple) -> bool:
+    if len(string) == 0:
+        if len(groups) == 0:
+            return 1
+        else:
+            return 0
+    if len(groups) == 0:
+        if "#" in string:
+            return 0
+        return 1
+
+    if sum(groups) + len(groups) - 1 > len(string):
+        return 0
+    if string[0] == ".":
+        return possible(string[1:], groups)
+    if string[0] == "#":
+        while groups[0] > 0:
+            if string[0] == ".":
+                return 0
+            string = string[1:]
+            groups = (groups[0] - 1,) + groups[1:]
+
+        groups = groups[1:]
+        if len(string) != 0:
+            if string[0] == "#":
+                return 0
+            string = "." + string[1:]
+            return possible(string, groups)
+
+        return possible(string, groups)
+
+    if string[0] == "?":
+        a = "#" + string[1:]
+        b = "." + string[1:]
+        return possible(a, groups) + possible(b, groups)
+
+
 def p1(lines):
-    def generate_combinations(length, blocks, partial, results):
-
-        if not blocks:
-            # If no more blocks to place, fill the rest with '.'
-            results.append(partial + '.' * (length - len(partial)))
-            return results
-
-        if len(partial) > 0:
-            # Add a separator '.' if this is not the first block
-            partial += '.'
-
-        for i in range(len(partial), length - sum(blocks) - len(blocks) + 2):
-            new_partial = partial + '.' * (i - len(partial)) + '#' * blocks[0]
-            generate_combinations(length, blocks[1:], new_partial, results)
-
-        return results
-
-
-    def get_arrangements(s, s_groups_):
-        a = 0
-        for c in generate_combinations(len(s), s_groups_, "", []):
-            for i in range(len(s)):
-                if c[i] == '.' and s[i] == '#' or s[i] == '.' and c[i] == '#':
-                    break
-            else:
-                a += 1
-
-        return a
-
-    print("")
     ans = 0
     for line in lines:
-        (symbols, s_groups) = line.split()
-        s_groups = [int(x) for x in s_groups.split(',')]
-        ans += get_arrangements(symbols, s_groups)
+        string, group = line.split(" ")
+        groups = tuple(int(x) for x in group.split(","))
+        ans += possible(string, groups)
 
+    return ans
+
+
+def p2(lines):
+    ans = 0
+    for idx, line in enumerate(lines):
+        string, group = line.split(" ")
+        string = "?".join([string] * 5)
+        group = ",".join([group] * 5)
+        groups = tuple(int(x) for x in group.split(","))
+        ans += possible(string, groups)
     return ans
